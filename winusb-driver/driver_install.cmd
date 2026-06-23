90132e4e54cb57a2d5c13ee5d86431e83769ad62
boringethan <ethan@openwater.cc>
2026-06-23 10:18:24 -0700

    docs: update driver_install.cmd comment (three -> four packages)

--- winusb-driver/driver_install.cmd
+++ winusb-driver/driver_install.cmd
@@ -6,7 +6,7 @@ rem Install the public certificate into Trusted Publishers and Root
 "%SystemRoot%\System32\certutil.exe" -addstore -f TrustedPublisher "%DIR%OpenMotion_signing_cert.cer" || exit /b 1
 "%SystemRoot%\System32\certutil.exe" -addstore -f Root             "%DIR%OpenMotion_signing_cert.cer" || exit /b 1
 
-rem Install all three driver packages
+rem Install all four driver packages
 "%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_0).inf" /install || exit /b 1
 "%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_1).inf" /install || exit /b 1
 "%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_2).inf" /install || exit /b 1
